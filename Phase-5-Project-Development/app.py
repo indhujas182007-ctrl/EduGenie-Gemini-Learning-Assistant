1c318dbea69149bec54bb9cfbad941f5f1c56dd0
indhujas182007-ctrl <indhujas182007@gmail.com>
2026-09-24 17:21:56 +0530

    Update app.py

--- Phase-5-Project-Development/app.py
+++ Phase-5-Project-Development/app.py
@@ -3,27 +3,52 @@ import os
 from fastapi import FastAPI, Form, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from google import genai
 
+
+# Create FastAPI application
 app = FastAPI(title="EduGenie")
 
-templates = Jinja2Templates(directory="Phase-5-Project-Development/templates")
 
+# Connect static CSS files
+app.mount(
+    "/static",
+    StaticFiles(directory="Phase-5-Project-Development/static"),
+    name="static"
+)
+
+
+# Templates folder
+templates = Jinja2Templates(
+    directory="Phase-5-Project-Development/templates"
+)
+
+
+# Connect Google Gemini
 client = genai.Client(
     api_key=os.getenv("GEMINI_API_KEY")
 )
 
 
+# Home page
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
+
     return templates.TemplateResponse(
         "index.html",
-        {"request": request}
+        {
+            "request": request
+        }
     )
 
 
+# Ask question
 @app.post("/ask", response_class=HTMLResponse)
-def ask_question(request: Request, question: str = Form(...)):
+def ask_question(
+    request: Request,
+    question: str = Form(...)
+):
 
     response = client.models.generate_content(
         model="gemini-3.6-flash",
